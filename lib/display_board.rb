e07a8a379d9cd674334256b9b3c979c0176ebfbb
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-07 12:47:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-
-def display_board
+board = ["X"," "," "," "," "," "," "," "," "]
+def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -9,4 +9,4 @@ def display_board
   puts "   |   |   "
 end
 
-display_board()
+display_board(0)
